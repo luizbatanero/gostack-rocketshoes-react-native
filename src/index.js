984c21f41702e3ebd9d0080860e6f1903605880f
Luiz Batanero <luizbatanero@gmail.com>
2019-07-10 02:27:35 -0300

    Add React Native Vector Icons

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React, { Fragment } from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import styled from 'styled-components';
 
 import './config/ReactotronConfig';
 
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
 const App = () => {
   return (
-    <View>
-      <Text>ReactNative</Text>
-    </View>
+    <Container>
+      <Icon name="shopping-cart" size={80} color="#999" />
+    </Container>
   );
 };
 
