67b2a2386beb38f2fdac7ef36b23b63ed32380a4
Luiz Batanero <luizbatanero@gmail.com>
2019-07-11 02:39:43 -0300

    Display empty cart message

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,13 +1,52 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { useSelector } from 'react-redux';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { rgba } from 'polished';
 
-// import { Container } from './styles';
+import { formatPrice } from '../../util/format';
+import { Container, Box, Title, Message, Button, ButtonText } from './styles';
 
-const Cart = () => {
+const Cart = ({ navigation }) => {
+  const products = useSelector(state =>
+    state.cart.map(product => ({
+      ...product,
+      subtotal: formatPrice(product.price * product.amount),
+    }))
+  );
+  const total = useSelector(state =>
+    formatPrice(
+      state.cart.reduce(
+        (total, product) => total + product.price * product.amount,
+        0
+      )
+    )
+  );
+
+  if (products.length) {
+    return (
+      <Container>
+        <Box>
+          <Title>TODO: CART</Title>
+        </Box>
+      </Container>
+    );
+  }
+
   return (
-    <View style={{ flex: 1, backgroundColor: '#7159c1' }}>
-      <Text>Cart</Text>
-    </View>
+    <Container>
+      <Box empty>
+        <Icon
+          name="remove-shopping-cart"
+          color={rgba(0, 0, 0, 0.1)}
+          size={128}
+        />
+        <Title>SHOPPING CART IS EMPTY</Title>
+        <Message>You have no products in your shopping cart.</Message>
+        <Button onPress={() => navigation.pop()}>
+          <ButtonText>CONTINUE SHOPPING</ButtonText>
+        </Button>
+      </Box>
+    </Container>
   );
 };
 
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background: #7159c1;
+`;
+
+export const Box = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  margin: 20px;
+  background: ${props => (props.empty ? 'rgba(0, 0, 0, 0.1)' : '#fff')};
+  border-radius: 3px;
+  padding: ${props => (props.empty ? '20px' : '10px')};
+  elevation: ${props => (props.empty ? 0 : 2)};
+`;
+
+export const Title = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  color: rgba(0, 0, 0, 0.5);
+  margin: 20px 0 5px;
+`;
+
+export const Message = styled.Text`
+  font-size: 13px;
+  color: rgba(0, 0, 0, 0.35);
+  margin-bottom: 25px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  background: #fff;
+  padding: 8px 12px;
+  border-radius: 3px;
+`;
+
+export const ButtonText = styled.Text`
+  color: #7159c1;
+  font-size: 13px;
+  font-weight: bold;
+  margin-right: 5px;
+`;
