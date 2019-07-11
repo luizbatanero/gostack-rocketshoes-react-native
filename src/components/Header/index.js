d50cc5dea96c6b0c9a88254171fbecb48ceef209
Luiz Batanero <luizbatanero@gmail.com>
2019-07-11 02:16:59 -0300

    Display cart size on Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Logo, LogoImage, CartButton, Badge } from './styles';
 
 const Header = ({ navigation }) => {
+  const cartSize = useSelector(state => state.cart.length);
+
   return (
     <Container>
       <Logo onPress={() => navigation.pop()}>
@@ -12,7 +15,7 @@ const Header = ({ navigation }) => {
       </Logo>
       <CartButton onPress={() => navigation.navigate('Cart')}>
         <Icon name="shopping-cart" size={28} color="#130042" />
-        <Badge>0</Badge>
+        <Badge>{cartSize || 0}</Badge>
       </CartButton>
     </Container>
   );
