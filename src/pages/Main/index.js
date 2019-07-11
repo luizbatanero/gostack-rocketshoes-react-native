f2b7abbb3d68cee940b0957f7aff1dbf9da0ea7a
Luiz Batanero <luizbatanero@gmail.com>
2019-07-11 02:10:41 -0300

    Display total

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { ActivityIndicator } from 'react-native';
+import { darken } from 'polished';
 
 import api from '../../services/api';
 import { formatPrice } from '../../util/format';
+
+import * as CartActions from '../../store/modules/cart/actions';
 
 import {
   Container,
@@ -15,13 +20,42 @@ import {
   ProductAmount,
   ProductAmountText,
   AddButtonText,
+  BottomBar,
+  EmptyCart,
+  EmptyCartMessage,
+  GoToCart,
+  GoToCartButton,
+  GoToCartButtonText,
+  TotalWrapper,
+  TotalLabel,
+  TotalValue,
 } from './styles';
 
 const Main = () => {
+  const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
+
+  const amount = useSelector(state =>
+    state.cart.reduce((amount, product) => {
+      amount[product.id] = product.amount;
+      return amount;
+    }, {})
+  );
+  const cartSize = useSelector(state => state.cart.length);
+  const total = useSelector(state =>
+    formatPrice(
+      state.cart.reduce((total, product) => {
+        return total + product.price * product.amount;
+      }, 0)
+    )
+  );
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchProducts = async () => {
+      setLoading(true);
+
       const response = await api.get('products');
 
       const data = response.data.map(product => ({
@@ -30,32 +64,73 @@ const Main = () => {
       }));
 
       setProducts(data);
+
+      setTimeout(() => {
+        setLoading(false);
+      }, 500);
     };
 
     fetchProducts();
   }, []);
 
+  const handleAddProduct = id => {
+    dispatch(CartActions.addToCartRequest(id));
+  };
+
+  if (loading) {
+    return (
+      <Container loading>
+        <ActivityIndicator color="#fff" size="large" />
+      </Container>
+    );
+  }
+
   return (
-    <Container>
-      <List
-        data={products}
-        keyExtractor={product => String(product.id)}
-        renderItem={({ item, index }) => (
-          <Product first={index === 0}>
-            <ProductImage source={{ uri: item.image }} />
-            <ProductTitle>{item.title}</ProductTitle>
-            <ProductPrice>{formatPrice(item.price)}</ProductPrice>
-            <AddButton onPress={() => this.handleAddProduct(item.id)}>
-              <ProductAmount>
-                <Icon name="add-shopping-cart" color="#FFF" size={20} />
-                <ProductAmountText>{0}</ProductAmountText>
-              </ProductAmount>
-              <AddButtonText>ADD TO CART</AddButtonText>
-            </AddButton>
-          </Product>
+    <>
+      <Container>
+        <List
+          data={products}
+          keyExtractor={product => String(product.id)}
+          renderItem={({ item, index }) => (
+            <Product first={index === 0}>
+              <ProductImage source={{ uri: item.image }} />
+              <ProductTitle>{item.title}</ProductTitle>
+              <ProductPrice>{formatPrice(item.price)}</ProductPrice>
+              <AddButton onPress={() => handleAddProduct(item.id)}>
+                <ProductAmount>
+                  <Icon name="add-shopping-cart" color="#FFF" size={20} />
+                  <ProductAmountText>{amount[item.id] || 0}</ProductAmountText>
+                </ProductAmount>
+                <AddButtonText>ADD TO CART</AddButtonText>
+              </AddButton>
+            </Product>
+          )}
+        />
+      </Container>
+      <BottomBar>
+        {cartSize > 0 ? (
+          <GoToCart>
+            <TotalWrapper>
+              <TotalLabel>TOTAL</TotalLabel>
+              <TotalValue>{total}</TotalValue>
+            </TotalWrapper>
+            <GoToCartButton>
+              <GoToCartButtonText>GO TO CART</GoToCartButtonText>
+              <Icon name="keyboard-arrow-right" color="#7159c1" size={18} />
+            </GoToCartButton>
+          </GoToCart>
+        ) : (
+          <EmptyCart>
+            <Icon
+              name="remove-shopping-cart"
+              color={darken(0.25, '#7159c1')}
+              size={36}
+            />
+            <EmptyCartMessage>Your cart is empty</EmptyCartMessage>
+          </EmptyCart>
         )}
-      />
-    </Container>
+      </BottomBar>
+    </>
   );
 };
 
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -4,6 +4,7 @@ import { darken } from 'polished';
 export const Container = styled.View`
   flex: 1;
   background: #7159c1;
+  padding-top: ${props => (props.loading ? '40px' : '0')};
 `;
 
 export const List = styled.FlatList``;
@@ -59,6 +60,63 @@ export const ProductAmountText = styled.Text`
 export const AddButtonText = styled.Text`
   flex: 1;
   text-align: center;
+  font-weight: bold;
+  color: #fff;
+`;
+
+export const BottomBar = styled.View`
+  height: 70px;
+  background: ${darken(0.2, '#7159c1')};
+  border-top-color: rgba(0, 0, 0, 0.05);
+  border-top-width: 2px;
+`;
+
+export const EmptyCart = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`;
+
+export const EmptyCartMessage = styled.Text`
+  margin-left: 10px;
+  font-size: 15px;
+  text-transform: uppercase;
+  color: rgba(0, 0, 0, 0.25);
+  font-weight: bold;
+`;
+
+export const GoToCart = styled.View`
+  padding: 20px;
+  flex-direction: row;
+  flex: 1;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const GoToCartButton = styled.TouchableOpacity`
+  background: #fff;
+  padding: 8px 12px;
+  border-radius: 3px;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const GoToCartButtonText = styled.Text`
+  color: #7159c1;
+  font-size: 13px;
+  font-weight: bold;
+  margin-right: 5px;
+`;
+
+export const TotalWrapper = styled.View``;
+
+export const TotalLabel = styled.Text`
+  font-size: 10px;
+  color: rgba(255, 255, 255, 0.5);
+`;
+export const TotalValue = styled.Text`
+  font-size: 20px;
   font-weight: bold;
   color: #fff;
 `;
